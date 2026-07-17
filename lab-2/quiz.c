c7cd25ca40e78c7cf2ab8d5a9b3eadcd18df844b
Vxtige1 <nameat1234@example.com>
2026-07-17 00:14:17 -0700

    lab-2

---
+++ lab-2/quiz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{ 
+    printf("%-13s: %s\n", "Programming", "A");
+    printf("%-13s: %s\n", "Drawing",   "B+");
+    printf("%-13s: %s\n", "Mechanics",   "B+");
+    printf("%-13s: %s\n", "Math",   "B");
+    printf("%-13s: %s\n", "GPS",   "3.5");
+
+    return 0;
+}
