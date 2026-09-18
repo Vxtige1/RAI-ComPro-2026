0d1e8b74711ca42b9c3a09961765033952123a2b
Vxtige1 <nameat1234@example.com>
2026-09-18 16:02:18 +0700

    lab-8

--- lab-8/ex03.c
+++ lab-8/ex03.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
-#include <ctype.h>
 
-void count_characters(const char *str, int *letters, int *digits, int *specials) {
-    *letters = 0;
-    *digits = 0;
-    *specials = 0;
+int sum_of_odd_squares(int start, int end) {
+    if (start > end) {
+        int temp = start;
+        start = end;
+        end = temp;
+    }
 
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (isalpha((unsigned char)str[i])) {
-            (*letters)++;
-        } else if (isdigit((unsigned char)str[i])) {
-            (*digits)++;
-        } else {
-            (*specials)++;
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+        if (i % 2 != 0) {
+            sum += (i * i);
         }
     }
+    return sum;
 }
 
 int main(void) {
-    const char input[] = "C-Pro 2026!";
-    int letters, digits, specials;
-
-    printf("Input: \"%s\"\n", input);
-    count_characters(input, &letters, &digits, &specials);
-
-    printf("Output:\n");
-    printf("Letters: %d\n", letters);
-    printf("Digits: %d\n", digits);
-    printf("Special Characters / Symbols: %d\n", specials);
-
+    int start, end;
+    printf("Enter the two integers (start end): ");
+    if (scanf("%d %d", &start, &end) == 2) {
+        int result = sum_of_odd_squares(start, end);
+        printf("Sum of squares of odd numbers from %d to %d is %d\n", start, end, result);
+    }
     return 0;
 }
